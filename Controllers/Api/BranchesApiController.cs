5aaa6e74e87dc2439618135e00e8e970bd819b83
GGolja <gg8283@student.uni-lj.si>
2023-01-09 18:57:24 +0100

    api1

---
+++ Controllers/Api/BranchesApiController.cs
@@ -0,0 +1,124 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Projektna.Data;
+using Projektna.Models;
+
+namespace Projektna.Controllers_Api
+{
+    [Route("api/v1/branch")]
+    [ApiController]
+    public class BranchesApiController : ControllerBase
+    {
+        private readonly StoreContext _context;
+
+        public BranchesApiController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/BranchesApi
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Branch>>> GetBranches()
+        {
+          if (_context.Branches == null)
+          {
+              return NotFound();
+          }
+            return await _context.Branches.ToListAsync();
+        }
+
+        // GET: api/BranchesApi/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Branch>> GetBranch(int id)
+        {
+          if (_context.Branches == null)
+          {
+              return NotFound();
+          }
+            var branch = await _context.Branches.FindAsync(id);
+
+            if (branch == null)
+            {
+                return NotFound();
+            }
+
+            return branch;
+        }
+
+        // PUT: api/BranchesApi/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBranch(int id, Branch branch)
+        {
+            if (id != branch.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(branch).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BranchExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/BranchesApi
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Branch>> PostBranch(Branch branch)
+        {
+          if (_context.Branches == null)
+          {
+              return Problem("Entity set 'StoreContext.Branches'  is null.");
+          }
+            _context.Branches.Add(branch);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBranch", new { id = branch.ID }, branch);
+        }
+
+        // DELETE: api/BranchesApi/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBranch(int id)
+        {
+            if (_context.Branches == null)
+            {
+                return NotFound();
+            }
+            var branch = await _context.Branches.FindAsync(id);
+            if (branch == null)
+            {
+                return NotFound();
+            }
+
+            _context.Branches.Remove(branch);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool BranchExists(int id)
+        {
+            return (_context.Branches?.Any(e => e.ID == id)).GetValueOrDefault();
+        }
+    }
+}
